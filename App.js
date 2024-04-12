e23cb74474b3e824af8c950b5cd80610367ffb6a
Dhruv Doshi <dhruv.doshi@somaiya.edu>
2024-04-12 11:37:16 -0400

    Final commit A6 v01

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
-import Hello from "./Hello.js"
+import Hello from "./Hello.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
-import express from 'express'
+import mongoose from "mongoose";
+import express from "express";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
+mongoose.connect(
+  process.env.DB_CONNECTION_STRING
+);
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
